ee5741512259a3def60ae15d314154cf15c29fa7
Chisato Murakami <kiwipom1031@gmail.com>
2020-08-15 10:51:42 -0700

    add top-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { PracticeComponent } from './practice/practice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    PracticeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: PracticeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
